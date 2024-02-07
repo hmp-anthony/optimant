fc3c19fc85d0f31362d5d58fc73d726d13d41cff
hmp-anthony <anthonygoddard962@gmail.com>
2024-02-07 17:53:11 +0000

    need to modify autodiff before moving on

--- optimant/gd.hpp
+++ optimant/gd.hpp
@@ -4,21 +4,6 @@
 
 using autodiff::base::var;
 
-/*
-template<std::size_t N>
-std::vector<double> gd(const std::vector<double> init,
-                       const double step_size,
-                       const int number_of_steps,
-                       const var fun) {
-
-    std::vector<var> var_vector;
-    for(int i = 0; i < number_of_steps; ++i) {
-        // obtain gradient of loff function.
-        auto C = autodiff::base::gradient(fun);
-        // take a step
-        a_ = a_ - step_size*C[a];
-        b_ = b_ - step_size*C[b];
-        std::cout << a_ << " " << b_ << std::endl;
-    }
-
-}*/
+void fun_manip(const var init_value, const var func) {
+    std::cout << "in fun_manip" << std::endl;
+}
--- optimant/tests/gd_test.cpp
+++ optimant/tests/gd_test.cpp
@@ -4,8 +4,14 @@
 
 #include "gtest/gtest.h"
 
+using autodiff::base::var;
+
 TEST(basic, gd) {
-
     ASSERT_EQ(1, 1);
+    var a(1);
+    var b(2);
 
+    auto c = (a + b) * b;
+
+    fun_manip(a, c);
 }
