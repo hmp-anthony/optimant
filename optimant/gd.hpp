dc1b1fd8abb173dd5a71cd044ad6814a83e4f34b
hmp-anthony <anthonygoddard962@gmail.com>
2024-11-12 15:36:21 +0000

    improved gradient descent

--- optimant/gd.hpp
+++ optimant/gd.hpp
@@ -5,19 +5,27 @@
 
 using autodiff::base::var;
 
-std::array<double, 2>  gd(std::array<var, 2>& v, var& eqn, double step_size, int num_steps) {
 
-    for(int i = 0; i < num_steps; ++i) {    
+template<int N>
+std::array<double, N>  gd(std::array<var, N>& v, var& eqn, double step_size, int num_steps) {
+
+    for(int i = 0; i < num_steps; ++i) {
+	
         auto G = autodiff::base::gradient(eqn);
-        auto c0 = v[0].value();
-        auto c1 = v[1].value();
+	std::array<double, N> c;
 
-        c0 = c0 - G[v[0]] * step_size;
-        c1 = c1 - G[v[1]] * step_size;
+	for(int i = 0; i < N; ++i) {
+            c[i] = v[i].value();
+            c[i] = c[i] - G[v[i]] * step_size;
+            set_value(v[i], c[i]);
+	}
 
-        set_value(v[0], c0);
-        set_value(v[1], c1);
+	eqn.forward_pass();
     }
-
-    return {v[0].value(), v[1].value()};
+    
+    std::array<double, N> output;
+    for(int i = 0; i < N; ++i) {
+	output[i] = v[i].value();
+    }
+    return output;
 }
--- optimant/tests/gd_test.cpp
+++ optimant/tests/gd_test.cpp
@@ -7,9 +7,10 @@
 using autodiff::base::var;
 
 TEST(basic, gd) {
-    std::array<var, 2> v = {var(9), var(2)};
-    auto eqn = v[0] * v[0] + v[1] * v[1];
-    auto min = gd(v, eqn, 0.1, 100);
-    ASSERT_NEAR(min[0], 0, 0.01);
-    ASSERT_NEAR(min[1], 0, 0.01);
+    std::array<var, 3> v = {var(9), var(2), var(100)};
+    auto eqn = (v[0] - 10) * (v[0] - 10) + (v[1] - 6) * (v[1] - 6) + v[2] * (v[2] - 3); 
+    auto min = gd<3>(v, eqn, 0.1, 100);
+    ASSERT_NEAR(min[0], 10, 0.01);
+    ASSERT_NEAR(min[1], 6, 0.01);
+    ASSERT_NEAR(min[2], 1.5, 0.01);
 }
